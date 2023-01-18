30184b1dca63372f94aee51b8ac2150f2c723d0d
unknown <tombirbeck47@gmail.com>
2023-01-18 04:01:12 +0000

    check answer function working

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ const answerThree = document.querySelector('.answerButtonThree')
 const answerFour = document.querySelector('.answerButtonFour')
 const question = document.querySelector('.questionText')
 
+const questions = async () => {
+    const res = await fetch('https://the-trivia-api.com/api/questions?limit=1&categories=science,history', {
+   headers: {
+     'Content-Type': 'application/json'
+   },
+ })
+ const data  = await res.json()
+ question.textContent = data[0].question
+ answerMixer(data[0].correctAnswer, data[0].incorrectAnswers[0], data[0].incorrectAnswers[1], data[0].incorrectAnswers[2]);
+ return data
+ }
+
+
 // take in 1 correct answer and 3 incorrect
 // jumble them and assign them to buttons
 
@@ -20,29 +33,29 @@ const answerMixer = (correctAnswer, incorrectOne, incorrectTwo, incorrectThree) 
 } while (mixed.length < 4);
 
 
-answerOne.textContent = mixed[0];   
-answerOne.nodeValue = mixed[0];    
-answerTwo.textContent = mixed[1];    
-answerTwo.nodeValue = mixed[1];     
-answerThree.textContent = mixed[2];    
-answerThree.nodeValue = mixed[2];    
-answerFour.textContent = mixed[3];   
-answerFour.nodeValue = mixed[3];    
+answerOne.textContent = mixed[0];
+answerTwo.textContent = mixed[1];       
+answerThree.textContent = mixed[2];      
+answerFour.textContent = mixed[3];     
 
+const checkAnswer = (e) => {
+    if (e.target.textContent === correctAnswer){
+        console.log(true)
+        return true
+    } else {
+        console.log(false)
+        return false
+    }
+}
+
+answerOne.addEventListener('click', checkAnswer)
+answerTwo.addEventListener('click', checkAnswer)
+answerThree.addEventListener('click', checkAnswer)
+answerFour.addEventListener('click', checkAnswer)
 };
 
 
-const questions = async () => {
-   const res = await fetch('https://the-trivia-api.com/api/questions?limit=1&categories=science,history', {
-  headers: {
-    'Content-Type': 'application/json'
-  },
-})
-const data  = await res.json()
-question.textContent = data[0].question
-answerMixer(data[0].correctAnswer, data[0].incorrectAnswers[0], data[0].incorrectAnswers[1], data[0].incorrectAnswers[2]);
-return data
 
-}
+
 
 questions()
